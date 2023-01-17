034ccfd839b770437594716cd21013dc6ce6fb74
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-17 07:44:55 +0530

    [Akshay]Added sample Nlog program

---
+++ SampleNlog/AddNumber.cs
@@ -0,0 +1,30 @@
+﻿using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleNlog
+{
+    public class AddNumber
+    {
+        NLog nlog=new NLog();
+       
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                nlog.LogDebug("Debug sucessfull : Sum()");
+                nlog.LogError("Expecting null variables");
+                nlog.LogWarn("a or b should nont be equal to 0");
+
+            }
+            int c = a + b;
+            nlog.LogDebug("Debug sucessfull");
+            nlog.LogInfo("Sum Method Passed" + c);
+            return c;
+        }
+
+    }
+}
---
+++ SampleNlog/Program.cs
@@ -0,0 +1,11 @@
+﻿using SampleNlog;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        AddNumber addNumber=new AddNumber();
+        int Result = addNumber.Sum(12, 13);
+        Console.WriteLine(Result);
+    }
+}
